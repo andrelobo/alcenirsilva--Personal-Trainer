22471098e2c8f675b925600c6c5d399f530e4407
Matthew-19 <113844755+Matthew-19@users.noreply.github.com>
2023-12-21 20:51:15 +0200

    update: menu and toggle theme icons with simple animation for navlist in small devices

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,6 +4,7 @@ import ToggleTheme from "./ToggleTheme/ToggleTheme";
 import "./ToggleTheme/ToggleTheme.css";
 import useScrollBlock from "../services/useScrollBlock";
 import { FiMenu } from "react-icons/fi";
+import { FaTimes } from "react-icons/fa";
 
 export default function Nav() {
   const { homeVisibility } = React.useContext(AppContext);
@@ -23,6 +24,12 @@ export default function Nav() {
   const windowWidth = window.innerWidth;
   const isBigWindow = windowWidth > 1200 ? true : false;
 
+  const menu_icon = showMenu ? (
+    <FaTimes onClick={toggleMenu} className="menu--icon" />
+  ) : (
+    <FiMenu onClick={toggleMenu} className="menu--icon" />
+  );
+
   return (
     <nav className={`${navClass}`}>
       <div className="container">
@@ -32,7 +39,7 @@ export default function Nav() {
           </a>
         </div>
         {(showMenu || isBigWindow) && (
-          <ul className="nav--list">
+          <ul className="nav--list slidein">
             <li>
               <a className="nav--link" href="#">
                 home
@@ -65,8 +72,14 @@ export default function Nav() {
             </li>
           </ul>
         )}
-        {(showMenu || isBigWindow) && <ToggleTheme />}
-        <FiMenu onClick={toggleMenu} className="menu--icon" />
+        {isBigWindow ? (
+          <ToggleTheme />
+        ) : (
+          <div className="nav--rightside-group">
+            <ToggleTheme />
+            {menu_icon}
+          </div>
+        )}
       </div>
     </nav>
   );
--- src/components/ToggleTheme/ToggleTheme.jsx
+++ src/components/ToggleTheme/ToggleTheme.jsx
@@ -3,14 +3,32 @@ import { FaRegSun, FaRegMoon } from "react-icons/fa6";
 import { AppContext } from "../../App";
 
 export default function ToggleTheme() {
-  const { toggleTheme } = React.useContext(AppContext);
-  return (
-    <div onClick={toggleTheme} className="toggle--theme">
+  const { toggleTheme, theme } = React.useContext(AppContext);
+  const windowWidth = window.innerWidth;
+  const isBigWindow = windowWidth > 1200 ? true : false;
+
+  const toggle_small =
+    theme === "light-theme" ? (
+      <FaRegSun className="sun--icon" />
+    ) : (
+      <FaRegMoon className="moon--icon" />
+    );
+
+  const toggle_icon = isBigWindow ? (
+    <>
       <FaRegSun className="sun--icon" />
       <div className="bar">
         <div className="circle"></div>
       </div>
       <FaRegMoon className="moon--icon" />
+    </>
+  ) : (
+    toggle_small
+  );
+
+  return (
+    <div onClick={toggleTheme} className="toggle--theme">
+      {toggle_icon}
     </div>
   );
 }

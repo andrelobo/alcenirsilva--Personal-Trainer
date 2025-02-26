bb18d1701a1dec37c7857fdc2cb36653cc2fcb11
André Lobo <loboandre@hotmail.com>
2025-02-26 07:58:24 -0400

    fix: vercel settings II

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Remova "/TrainingStudio/" para testar
+  base: "./", // Usa "./" para compatibilidade com caminhos relativos
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+  server: {
+    open: true,
+  },
 });
